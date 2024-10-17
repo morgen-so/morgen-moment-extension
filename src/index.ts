d504234cd1044403dc8db09a11df2a1c0f4c5069
Marco Ancona <marco@morgen.so>
2024-10-17 18:59:15 +0200

    add some comments

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,20 @@ import moment from "moment-timezone";
 
 declare module "moment-timezone" {
   interface Moment {
+    /**
+     * Format as YYYY-MM-DD
+     * @returns
+     */
     formatISODate: () => string;
+    /**
+     * Format as YYYY-MM-DDTHH:mm:ssZ, where Z is "Z" for UTC-equivalent, or "+-hh:mm" otherwise
+     * @returns
+     */
     formatISODateTime: () => string;
+    /**
+     * Format as YYYY-MM-DDTHH:mm:ss, thus omitting the time zone offset
+     * @returns
+     */
     formatISODateTimeFloating: () => string;
   }
 }
